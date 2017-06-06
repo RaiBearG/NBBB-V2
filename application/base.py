dd412747a6a8163d31aa3bb13c2ccc34e40a85b8
Rai Gohalwar <raigohalwar@gmail.com>
2017-06-06 01:56:56 -0700

    working IMU retreiver

--- application/base.py
+++ application/base.py
@@ -13,10 +13,26 @@ from sbp.navigation import SBP_MSG_POS_LLH, MsgPosLLH
 import csv, time, math, socket, pickle, serial
 import numpy as np
 
-SERIAL_BAUD = 57600
+SERIAL_BAUD_IN = 57600
+SERIAL_BAUD_OUT = 115200
 DEFAULT_UDP_ADDRESS = '127.0.0.1' 
 DEFAULT_UDP_PORT = 13320 
 
+#open udp port 
+sock = socket.socket(socket.AF_INET,		#Internet
+					 socket.SOCK_DGRAM)		# UDP
+
+#open input serial link
+ser_in = serial.Serial(
+		port= '/dev/ttyUSB0',
+		baudrate= SERIAL_BAUD_IN
+		)
+
+#open output serial link
+ser_out = serial.Serial(
+		port= '/dev/ttyUSB1',
+		baudrate = SERIAL_BAUD_OUT
+		)
 
 
 def write_data(lat, lon, flag, sat, imu_x, imu_y):
@@ -27,3 +43,6 @@ def write_data(lat, lon, flag, sat, imu_x, imu_y):
 	data = lat, lon, flag, sat, imu_x, imu_y
 	package= pickle.dumps(data)
 	ser_out.write(package)
+	sock.sendto(package, (address,port))
+	return 
+
